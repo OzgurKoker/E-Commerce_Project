b289e63067a9feda5649c8a133129c49f480c967
Umutyolacan19 <emr.umut03@gmail.com>
2023-05-17 14:50:43 +0300

    BLL

---
+++ ETicaretApp/ETicaretApp.BLL/Abstract/IProductService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretApp.BLL.Abstract
+{
+    public interface IProductService
+    {
+        Product GetProductDetails(int id);
+        ProductListWM GetAll();
+        List<Product> GetPopularMethod();
+        ProductListWM GetProductsByCategory(string category);
+        Product GetById(int id);
+        void Create(Product entity);
+        void Update(Product entity);
+        void Delete(Product entity);
+    }
+}
