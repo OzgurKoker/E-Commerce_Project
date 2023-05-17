fbab61e6dcc2ad9bba2b435f004b226da87d1669
aysegul <aysegulkkeskin@gmail.com>
2023-05-17 15:27:55 +0300

    kategori classı eklendi

---
+++ ETicaretApp/ETicaretApp.Entities/Category.cs
@@ -0,0 +1,23 @@
+﻿using ETicaretApp.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretApp.Entities
+{
+    [Table("Categories")]
+    public class Category:EntityBase
+    {
+        [DisplayName("Kategori Adı")]
+        [MaxLength(50,ErrorMessage ="Kategori adı alanı 50 karakterden fazla olamaz.")]
+        [Required(ErrorMessage = "Kategori adı alanı boş geçilemez")]
+        public string Name { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
