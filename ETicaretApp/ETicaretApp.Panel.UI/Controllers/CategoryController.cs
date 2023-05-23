21a1967db73aa7b2225db685c4a2daca689cdb76
OzgurKoker <118525759+OzgurKoker@users.noreply.github.com>
2023-05-23 17:08:14 +0300

    kategori listesi

--- ETicaretApp/ETicaretApp.Panel.UI/Controllers/CategoryController.cs
+++ ETicaretApp/ETicaretApp.Panel.UI/Controllers/CategoryController.cs
@@ -1,6 +1,8 @@
 ﻿using ETicaretApp.BLL;
 using ETicaretApp.DAL.EntityFramework;
 using ETicaretApp.Entities;
+using ETicaretApp.Panel.UI.Models;
+using ETicaretApp.Panel.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -9,17 +11,49 @@ namespace ETicaretApp.Panel.UI.Controllers
     public class CategoryController : Controller
     {
         CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
-
+        private readonly INotificationService notificationService;
+        public CategoryController(INotificationService notificationService)
+        {
+            this.notificationService = notificationService;
+        }
 
         public IActionResult Index()
         {
-            List<Category> categoryList =  categoryManager.ListAll();
+            List<Category> categoryList = categoryManager.ListAll();
             return View(categoryList);
         }
         public IActionResult CreateCategoryPartial()
         {
-            ViewBag.Category = new SelectList(categoryManager.ListAll(), "Id", "Name");
+            ViewBag.Category = new SelectList(categoryManager.ListAll().Where(x=>x.CategoryId==null), "Id", "Name");
             return PartialView("_CreateCategoryPartialView");
+        }
+        [HttpPost]
+        public IActionResult Create(CategoryViewModel ctgView)
+        {
+            if (ctgView.check)
+                ctgView.CategoryId = null;
+
+
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    categoryManager.Create(new Category { Name = ctgView.Name,CategoryId=ctgView.CategoryId });
+                    notificationService.Notification(NotifyType.Success, $"{ctgView.Name} İsimli Kategori Başarılı Bir Şekilde Oluşturuldu");
+                }
+                catch (Exception ex) 
+                {
+                    notificationService.Notification(NotifyType.Error, ex.Message);
+                }
+            }
+            else
+                ModelStateControl.KontrolEt(notificationService, ModelState);
+
+
+
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ ETicaretApp/ETicaretApp.Panel.UI/Models/CategoryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace ETicaretApp.Panel.UI.Models
+{
+    public class CategoryViewModel
+    {
+        [DisplayName("Kategori Adı")]
+        [MaxLength(50, ErrorMessage = "Kategori adı alanı 50 karakterden fazla olamaz.")]
+        [Required(ErrorMessage = "Kategori adı alanı boş geçilemez")]
+        public string Name { get; set; }
+
+        [DisplayName("Ana Kategori")]
+        public int? CategoryId { get; set; }
+
+        public bool check { get; set; }
+    }
+}
