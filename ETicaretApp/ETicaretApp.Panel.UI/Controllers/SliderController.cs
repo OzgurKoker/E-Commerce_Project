91242867db1b43f64568424e612f01c4a199c962
aysegul <aysegulkkeskin@gmail.com>
2023-06-02 19:58:34 +0300

    Slider update.

---
+++ ETicaretApp/ETicaretApp.Panel.UI/Controllers/SliderController.cs
@@ -0,0 +1,220 @@
+﻿using ETicaretApp.BLL;
+using ETicaretApp.DAL.EntityFramework;
+using ETicaretApp.Entities;
+using ETicaretApp.Panel.UI.Models;
+using ETicaretApp.Panel.UI.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretApp.Panel.UI.Controllers
+{
+    [Authorize]
+    public class SliderController : Controller
+    {
+        SliderManager sliderManager = new SliderManager(new EfSliderRepository());
+        private readonly INotificationService notificationService;
+        private readonly IWebHostEnvironment webHostEnvironment;
+        public SliderController(INotificationService notificationService,IWebHostEnvironment webHostEnvironment)
+        {
+            this.webHostEnvironment = webHostEnvironment;
+            this.notificationService = notificationService;
+        }
+
+
+        public IActionResult Index()
+        {
+            List<Slider> sliderList = sliderManager.ListAll();
+            return View(sliderList);
+        }
+
+
+
+        public IActionResult CreateSliderPartial()
+        {
+            return PartialView("_CreateSliderPartialView");
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Create(SliderViewModel slider,IFormFile photo)
+        {
+            try
+            {
+                if (photo != null)
+                {
+                    string[] allowedExtensions = { ".jpg", ".jpeg", ".png" };
+                    string fileExtension = Path.GetExtension(photo.FileName).ToLower();
+
+                    if (allowedExtensions.Contains(fileExtension))
+                    {
+                        int fileSizeLimit = 10 * 1024 * 1024;
+
+                        if (photo.Length <= fileSizeLimit)
+                        {
+                            string wwwrootPath = webHostEnvironment.WebRootPath;
+                            string fileName = Path.GetFileNameWithoutExtension(photo.FileName);
+                            string extension = Path.GetExtension(photo.FileName);
+
+                            string newFileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                            string path = Path.Combine(wwwrootPath + "/img/Slider/", newFileName);
+
+                            using (var fileStream = new FileStream(path, FileMode.Create))
+                            {
+                                photo.CopyTo(fileStream);
+                            }
+
+                            sliderManager.Create(new Slider()
+                            {
+                                Image = newFileName,
+                                MainTittle=slider.MainTittle,
+                                SmallTittle=slider.SmallTittle,
+                                Url=slider.Url
+
+
+                            }) ;
+                            notificationService.Notification(NotifyType.Success, "Başarıyla fotoğraf eklendi.");
+
+                            return RedirectToAction(nameof(Index));
+                        }
+                        else
+                        {
+                            notificationService.Notification(NotifyType.Error, "Dosya boyutu limiti aşıldı (10mb)");
+                            return RedirectToAction(nameof(Index));
+                        }
+                    }
+                    else
+                    {
+                        notificationService.Notification(NotifyType.Error, "Geçersiz Dosya Formatı (jpg,jpeg,png) Seçiniz.");
+                        return RedirectToAction(nameof(Index));
+
+                    }
+                }
+                else
+                {
+                    notificationService.Notification(NotifyType.Error, "Fotoğraf Seçimi Zorunludur.");
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                notificationService.Notification(NotifyType.Error, ex.Message);
+
+            }
+
+
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        public IActionResult DeleteSliderPartial(int id)
+        {
+            Slider slider = sliderManager.GetById(id);
+
+
+            return PartialView("_DeleteSliderPartialView", slider);
+
+        }
+        [HttpPost]
+        public IActionResult Delete(Slider slider)
+        {
+
+            try
+            {
+                sliderManager.Delete(slider);
+                notificationService.Notification(NotifyType.Success, $"Fotoğraf silindi.");
+            }
+            catch (Exception ex)
+            {
+
+                notificationService.Notification(NotifyType.Error, ex.Message);
+            }
+
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult EditSliderPartial(int id)
+        {
+            Slider slider = sliderManager.GetById(id);
+            return PartialView("_EditSliderPartialView", slider);
+
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Slider slider, IFormFile photo)
+        {
+            try
+            {
+                if (photo != null)
+                {
+                    string[] allowedExtensions = { ".jpg", ".jpeg", ".png" };
+                    string fileExtension = Path.GetExtension(photo.FileName).ToLower();
+
+                    if (allowedExtensions.Contains(fileExtension))
+                    {
+                        int fileSizeLimit = 10 * 1024 * 1024;
+
+                        if (photo.Length <= fileSizeLimit)
+                        {
+                            string wwwrootPath = webHostEnvironment.WebRootPath;
+                            string fileName = Path.GetFileNameWithoutExtension(photo.FileName);
+                            string extension = Path.GetExtension(photo.FileName);
+
+                            string newFileName = slider.Image;
+                            string path = Path.Combine(wwwrootPath + "/img/Slider/", newFileName);
+
+                            using (var fileStream = new FileStream(path, FileMode.Create))
+                            {
+                                photo.CopyTo(fileStream);
+                            }
+
+                            sliderManager.Update(new Slider()
+                            {
+                                Image = newFileName,
+                                MainTittle = slider.MainTittle,
+                                SmallTittle = slider.SmallTittle,
+                                Url = slider.Url
+
+
+                            });
+                            notificationService.Notification(NotifyType.Success, "Başarıyla fotoğraf eklendi.");
+
+                            return RedirectToAction(nameof(Index));
+                        }
+                        else
+                        {
+                            notificationService.Notification(NotifyType.Error, "Dosya boyutu limiti aşıldı (10mb)");
+                            return RedirectToAction(nameof(Index));
+                        }
+                    }
+                    else
+                    {
+                        notificationService.Notification(NotifyType.Error, "Geçersiz Dosya Formatı (jpg,jpeg,png) Seçiniz.");
+                        return RedirectToAction(nameof(Index));
+
+                    }
+                }
+                else
+                {
+                    notificationService.Notification(NotifyType.Error, "Fotoğraf Seçimi Zorunludur.");
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                notificationService.Notification(NotifyType.Error, ex.Message);
+
+            }
+
+
+            return RedirectToAction(nameof(Index));
+
+        }
+
+
+    }
+}
---
+++ ETicaretApp/ETicaretApp.Panel.UI/Models/SliderViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using ETicaretApp.Entities.Abstract;
+
+namespace ETicaretApp.Panel.UI.Models
+{
+    public class SliderViewModel:EntityBase
+    {
+        [DisplayName("Fotoğraf")]
+        [Required(ErrorMessage = "Fotoğraf alanı boş geçilemez")]
+        public string Image { get; set; }
+
+
+        [DisplayName("Küçük Başlık")]
+        [MaxLength(50, ErrorMessage = "Başlık 50 karakterden fazla olamaz")]
+        [Required(ErrorMessage = "Küçük başlık boş geçilemez")]
+        public string SmallTittle { get; set; }
+
+
+        [DisplayName("Ana Başlık")]
+        [MaxLength(20, ErrorMessage = "Başlık 20 karakterden fazla olamaz")]
+        [Required(ErrorMessage = "Ana başlık boş geçilemez")]
+        public string MainTittle { get; set; }
+
+
+        [DisplayName("Buton Url")]
+        [Required(ErrorMessage = "Url alanı boş geçilemez")]
+        public string Url { get; set; }
+    }
+}
