f14b81fe3f5f1409ae6e6abacfec1918ea1e6f95
aysegul <aysegulkkeskin@gmail.com>
2023-06-02 17:10:28 +0300

    Login ,register ve şifremi unuttum sayfaları yapıldı.

---
+++ ETicaretApp/ETicaretApp.UI/Controllers/MemberController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretApp.UI.Controllers
+{
+    public class MemberController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+    }
+}
