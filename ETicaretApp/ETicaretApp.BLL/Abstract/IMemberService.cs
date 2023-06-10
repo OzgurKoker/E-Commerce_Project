f15424a0d57d86d7d3895212a3244c177429116e
OzgurKoker <118525759+OzgurKoker@users.noreply.github.com>
2023-06-10 18:52:06 +0300

    addedMemberManager

---
+++ ETicaretApp/ETicaretApp.BLL/Abstract/IMemberService.cs
@@ -0,0 +1,19 @@
+﻿using ETicaretApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretApp.BLL.Abstract
+{
+    public interface IMemberService
+    {
+        void Create(Member member);
+        void Update(Member member);
+        void Delete(Member member);
+        Member GetById(int id);
+        List<Member> ListAll();
+        IQueryable<Member> Query();
+    }
+}
---
+++ ETicaretApp/ETicaretApp.BLL/MemberManager.cs
@@ -0,0 +1,50 @@
+﻿using ETicaretApp.BLL.Abstract;
+using ETicaretApp.DAL.Abstract;
+using ETicaretApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretApp.BLL
+{
+    public class MemberManager : IMemberService
+    {
+        IMemberDal _memberDal;
+        public MemberManager(IMemberDal memberDal)
+        {
+            _memberDal = memberDal;
+        }
+
+        public void Create(Member member)
+        {
+            _memberDal.Create(member);
+        }
+
+        public void Delete(Member member)
+        {
+            _memberDal.Delete(member);
+        }
+
+        public Member GetById(int id)
+        {
+          return _memberDal.GetById(id);
+        }
+
+        public List<Member> ListAll()
+        {
+           return _memberDal.ListAll();
+        }
+
+        public IQueryable<Member> Query()
+        {
+            return _memberDal.Query();
+        }
+
+        public void Update(Member member)
+        {
+            _memberDal.Update(member);
+        }
+    }
+}
