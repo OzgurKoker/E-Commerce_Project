1d221889b6345c64ce3d4f0673984d0bc9a3f8db
OzgurKoker <118525759+OzgurKoker@users.noreply.github.com>
2023-06-14 09:57:48 +0300

    MenuPartial

---
+++ ETicaretApp/ETicaretApp.Panel.UI/Controllers/MemberController.cs
@@ -0,0 +1,39 @@
+﻿using ETicaretApp.BLL;
+using ETicaretApp.DAL.EntityFramework;
+using ETicaretApp.Entities;
+using ETicaretApp.Panel.UI.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretApp.Panel.UI.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class MemberController : Controller
+    {
+        MemberManager memberManager = new MemberManager(new EfMemberRepository());
+        private readonly INotificationService notificationService;
+
+        public MemberController(INotificationService notificationService)
+        {
+            this.notificationService = notificationService;
+        }
+
+        public IActionResult Index()
+        {
+            List<Member> memberList=memberManager.ListAll();
+            return View(memberList);
+        }
+
+        //[HttpPost]
+        //public IActionResult UpdateMemberState(string id)
+        //{
+        //    var member = memberManager.GetById(id);
+        //    member.State = !member.State;
+        //    memberManager.Update(member);
+        //    notificationService.Notification(NotifyType.Success, $"{member.Email} Mail adresli kullanıcı güncellendi.");
+
+        //    return Ok("Üye durumu güncellendi..");
+        //}
+    }
+}
