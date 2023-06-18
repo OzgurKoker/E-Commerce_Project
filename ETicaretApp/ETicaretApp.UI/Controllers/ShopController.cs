979abc171cc185b7be58237670e3aa03c4ff82ab
OzgurKoker <118525759+OzgurKoker@users.noreply.github.com>
2023-06-18 15:46:27 +0300

    Shop Fix

--- ETicaretApp/ETicaretApp.UI/Controllers/ShopController.cs
+++ ETicaretApp/ETicaretApp.UI/Controllers/ShopController.cs
@@ -2,18 +2,24 @@
 using ETicaretApp.DAL.EntityFramework;
 using ETicaretApp.UI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ETicaretApp.UI.Controllers
 {
     public class ShopController : Controller
     {
         CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
-
+        ProductManager productManager = new ProductManager(new EfProductRepository());
+        ProductImageManager productImageManager = new ProductImageManager(new EfProductImageRepository());
+        SliderManager sliderManager = new SliderManager(new EfSliderRepository());
         public IActionResult Index()
         {
             var model = new IndexViewModel()
             {
-                Categories = categoryManager.ListAll()
+                Categories = categoryManager.ListAll(),
+                Products = productManager.Query().Include(x => x.Brand).Where(x => x.State == true).OrderByDescending(x => x.CreatedDate).Take(8),
+                ProductImages = productImageManager.ListAll(),
+                Sliders = sliderManager.ListAll()
             };
             return View(model);
         }
