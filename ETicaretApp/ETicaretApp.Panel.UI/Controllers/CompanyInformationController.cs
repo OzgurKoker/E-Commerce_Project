797d85652cfa4f8276b965d937584901b92bffad
aysegul <aysegulkkeskin@gmail.com>
2023-06-22 16:26:26 +0300

    company view controller oluşturuldu

---
+++ ETicaretApp/ETicaretApp.Panel.UI/Controllers/CompanyInformationController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretApp.Panel.UI.Controllers
+{
+    [Authorize(Roles ="Admin")]
+    public class CompanyInformationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
